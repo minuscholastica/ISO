8a2d814883acb868cf5026d796ac7d2177c57c92
Minu <63788935+minuscholastica@users.noreply.github.com>
2024-06-02 11:56:19 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ISO
+`curl -fsSL https://bun.sh/install | bash # Install bun for macOS, Linux, and WSL`
+
+`bun install`
+
+`bun dev`
+
+Change things in `pages` to see the effect.
---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>Minu - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="Minu" />
+          <meta property="og:description" content="Minu is talking, to her friend Benjiee, regarding her successful submission of ISO 13485 software as medical certification in her previous company Nervotec. " />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://github.com/minuscholastica/minunamu/blob/main/Minune.png" />
+        </>
+      ),
+    };
+    
